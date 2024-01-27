b79248717b3b724270c843f8a442ee69fe433600
Ash <aeonincarnate@gmail.com>
2024-01-26 22:57:21 -0500

    Inertia, constants
    Add inertia and make constants available for editing

--- scripts/Comedian.gd
+++ scripts/Comedian.gd
@@ -1,11 +1,14 @@
 extends CharacterBody3D
 
-const SPEED = 0.5
-const JUMP_VELOCITY = 0.7
+@export var ACCEL = 0.3
+@export var SPEED = 0.7
+@export var JUMP_VELOCITY = 1.0
+@export var INERTIA = 0.05
+
+@onready var sprite = get_node("Sprite")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,13 +21,18 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_dir = Input.get_vector("move_right", "move_left", "move_down", "move_up")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	velocity.x += direction.x * ACCEL
+	velocity.z += direction.z * ACCEL
+	velocity.x = clamp(velocity.x, -SPEED, SPEED)
+	velocity.z = clamp(velocity.z, -SPEED, SPEED)
+	velocity.x = move_toward(velocity.x, 0, INERTIA)
+	velocity.z = move_toward(velocity.z, 0, INERTIA)
+	
+	if (velocity.x == 0 and velocity.z == 0):
+		sprite.stop()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		sprite.play('new_animation');
 
 	move_and_slide()

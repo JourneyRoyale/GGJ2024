d29a5e4b03a9c894b08e826549dd9a1c6722fdd6
Journey Jennings <journeyrj111@gmail.com>
2024-01-27 13:05:31 -0500

    SpotLight particles and collision

--- scripts/Game/SpotLight.gd
+++ scripts/Game/SpotLight.gd
@@ -1,10 +1,11 @@
-extends SpotLight3D
+extends Node3D
 
 var target_position = Vector3(1, 0.512, 1);
 var target_x_range = [-0.367, 0.336]
 var target_z_range = [-0.129, -0.5]
 # TODO: replace with singleton RNG
 var rng = RandomNumberGenerator.new()
+@onready var particles : CPUParticles3D = get_node("Ground/CPUParticles3D")
 
 func _ready():
 	select_new_target_position()
@@ -19,3 +20,18 @@ func _process(delta):
 	if (position.distance_to(target_position) < 0.01):
 		#TODO: wait
 		select_new_target_position()
+		
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):
+		particles.emitting = true
+		body.on_spotlight_entered()
+	pass # Replace with function body.
+
+
+func _on_area_3d_body_exited(body):
+	if body.is_in_group("Player"):
+		particles.emitting = false
+		body.on_spotlight_exited()
+	pass # Replace with function body.
